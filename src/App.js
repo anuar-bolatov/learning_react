458e1cca89d0ab32c0babec8b8a745642a2baa8a
Anuar Bolatov <anuar.bolatov@sage.com>
2022-02-07 00:37:44 +0000

    Added List component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import User from './user';
+import List from './List';
+
+const listItems = [
+  {listContent: 'Dog'},
+  {listContent: 'Cat'},
+  {listContent: 'Chicken'},
+  {listContent: 'Cow'},
+  {listContent: 'Horse'}
+];
 
 function App() {
   return (
     <div className="App">
       <div className="User" >
         <User login="anuarbolatov" />
+        <br />
+        <List listItems={listItems}/>
       </div>
     </div>
   );
